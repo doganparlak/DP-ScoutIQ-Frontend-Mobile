58525ecc84170c7e224ce4d04bfafa5b5c6df5c8
doganparlak <dgnprlk@gmail.com>
2026-04-02 13:05:45 +0300

    update

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ import { Platform } from 'react-native';
 
 // Change this IP to your Mac's local IP:
 //const LOCAL_DEV_API = 'http://192.168.1.114:8000';
-//const LOCAL_DEV_API = 'http://localhost:8000';
-const LOCAL_DEV_API = 'https://dp-scoutiq-backend-mobile.onrender.com';
+const LOCAL_DEV_API = 'http://localhost:8000';
+//const LOCAL_DEV_API = 'https://dp-scoutiq-backend-mobile.onrender.com';
 const PROD_API = 'https://dp-scoutiq-backend-mobile.onrender.com';
 
 export const API_BASE_URL =
@@ -12,7 +12,7 @@ export const API_BASE_URL =
     ? LOCAL_DEV_API   // when running in development (Expo Go, metro)
     : PROD_API;       // when building production
 // Android Emulator (Expo):
-// export const API_BASE_URL = 'http://10.0.2.2:8000';
+//export const API_BASE_URL = 'http://10.0.2.2:8000';
 //console.log('🌐 API BASE URL:', API_BASE_URL, '| DEV:', __DEV__);
 // Endpoints (adjust to match your backend; see notes in README)
 export const ENDPOINTS = {
